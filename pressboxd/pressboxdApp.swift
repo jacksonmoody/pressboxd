3668fecb8a13db87bb48ad99ef33ca827a113eb4
Jackson <jackson@jacksonmoody.com>
2024-06-22 15:06:05 -0400

    Initial Commit

---
+++ pressboxd/pressboxdApp.swift
@@ -0,0 +1,17 @@
+//
+//  pressboxdApp.swift
+//  pressboxd
+//
+//  Created by Jackson Moody on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct pressboxdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
